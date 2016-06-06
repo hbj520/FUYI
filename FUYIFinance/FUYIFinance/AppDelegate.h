0312d7ebfb78222c16187aa39a100a8fba339c38
hbj520 <hbjmailformac@163.com>
2016-06-06 13:50:11 +0800

    first commit

---
+++ FUYIFinance/FUYIFinance/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FUYIFinance
+//
+//  Created by youyou on 16/6/6.
+//  Copyright © 2016年 youyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FUYIFinance/FUYIFinance/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FUYIFinance
+//
+//  Created by youyou on 16/6/6.
+//  Copyright © 2016年 youyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
