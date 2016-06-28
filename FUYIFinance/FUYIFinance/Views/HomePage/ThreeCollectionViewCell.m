6c77426f943b6faaeff32544da422b5c44498c63
hbj520 <hbjmailformac@163.com>
2016-06-28 09:19:24 +0800

    655--han

--- FUYIFinance/FUYIFinance/Views/HomePage/ThreeCollectionViewCell.m
+++ FUYIFinance/FUYIFinance/Views/HomePage/ThreeCollectionViewCell.m
@@ -8,18 +8,18 @@
 
 #import "ThreeCollectionViewCell.h"
 #import <SDWebImage/UIImageView+WebCache.h>
-#import "HomepageExchangeModel.h"
+//#import "HomepageExchangeModel.h"
 @implementation ThreeCollectionViewCell
 
 - (void)awakeFromNib {
     // Initialization code
 }
 - (void)configWithData:(HomepageExchangeModel *)exchangeModel{
-    [self.imageView sd_setImageWithURL:[NSURL URLWithString:exchangeModel.exchangeImageurl] completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
-        UIImage *img = [Tools imageCompressForSize:image targetSize:CGSizeMake(66, 66)];
-        self.imageView.image = img;
-    }];
-
-    self.titleLable.text = exchangeModel.exchangName;
+//    [self.imageView sd_setImageWithURL:[NSURL URLWithString:exchangeModel.exchangeImageurl] completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
+//        UIImage *img = [Tools imageCompressForSize:image targetSize:CGSizeMake(66, 66)];
+//        self.imageView.image = img;
+//    }];
+//
+//    self.titleLable.text = exchangeModel.exchangName;
 }
 @end
