2cbf128f73cbbb254de016e73e185507a61dda67
zhangzhe <1659164165@qq.com>
2016-07-04 10:57:50 +0800

    个人界面-张

---
+++ FUYIFinance/FUYIFinance/Views/FourBtnTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  FourBtnTableViewCell.h
+//  FUYIFinance
+//
+//  Created by 张哲 on 16/7/4.
+//  Copyright © 2016年 youyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FourBtnTableViewCell : UITableViewCell
+
+@end
---
+++ FUYIFinance/FUYIFinance/Views/FourBtnTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  FourBtnTableViewCell.m
+//  FUYIFinance
+//
+//  Created by 张哲 on 16/7/4.
+//  Copyright © 2016年 youyou. All rights reserved.
+//
+
+#import "FourBtnTableViewCell.h"
+
+@implementation FourBtnTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
