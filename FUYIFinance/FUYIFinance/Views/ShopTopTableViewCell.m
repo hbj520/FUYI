0bd598d246e6a355d26ca6a24a93181bedc40b6c
zhangzhe <1659164165@qq.com>
2016-07-04 09:56:03 +0800

    搭建个人界面。-张

---
+++ FUYIFinance/FUYIFinance/Views/ShopTopTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ShopTopTableViewCell.m
+//  FUYIFinance
+//
+//  Created by 张哲 on 16/7/4.
+//  Copyright © 2016年 youyou. All rights reserved.
+//
+
+#import "ShopTopTableViewCell.h"
+
+@implementation ShopTopTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
