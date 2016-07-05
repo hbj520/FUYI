55193ddc2c519270884107ec6ef6aab33be2dfde
hbj520 <hbjmailformac@163.com>
2016-07-05 14:59:19 +0800

    添加接口类--han

--- FUYIFinance/FUYIFinance/Util/API/MyAPI.h
+++ FUYIFinance/FUYIFinance/Util/API/MyAPI.h
@@ -7,7 +7,24 @@
 //
 
 #import <Foundation/Foundation.h>
-
+typedef void (^VoidBlock) (void);
+typedef void (^StateBlock) (BOOL sucess, NSString *msg);
+typedef void (^ModelBlock) (BOOL success, NSString *msg, id object);
+typedef void (^ArrayBlock) (BOOL success, NSString *msg, NSMutableArray *arrays);
+typedef void (^ErrorBlock) (NSError *enginerError);
 @interface MyAPI : NSObject
++ (MyAPI *)sharedAPI;
+//取消所有网路全部请求
+- (void)cancelAllOperation;
+
+#pragma mark -富谊财经接口
+#pragma mark -注册/登陆/密码修改
+#pragma mark -商城
+#pragma mark -博客
+#pragma mark -个人中心
+#pragma mark -讲师团队
+
+
+
 
 @end
--- FUYIFinance/FUYIFinance/Util/API/MyAPI.m
+++ FUYIFinance/FUYIFinance/Util/API/MyAPI.m
@@ -5,9 +5,34 @@
 //  Created by youyou on 16/7/5.
 //  Copyright © 2016年 youyou. All rights reserved.
 //
-
+#import <AFNetworking/AFNetworking.h>
+#import <AFNetworking/AFURLResponseSerialization.h>
 #import "MyAPI.h"
 
-@implementation MyAPI
+@interface MyAPI ()
+@property (nonatomic, strong) AFHTTPRequestOperationManager *manager;
 
+@end
+@implementation MyAPI
+- (id)init{
+    self = [super init];
+    if (self) {
+        
+        self.manager = [[AFHTTPRequestOperationManager alloc]initWithBaseURL:[NSURL URLWithString:BaseUrl]] ;
+        self.manager.responseSerializer.acceptableContentTypes =  [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript",@"text/html", nil];
+        self.manager.requestSerializer = [AFHTTPRequestSerializer serializer];
+    }
+    return self;
+}
++ (MyAPI *)sharedAPI{
+    static MyAPI *sharedAPIInstance;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedAPIInstance = [[self alloc] init];
+    });
+    return sharedAPIInstance;
+}
+- (void)cancelAllOperation{
+    [self.manager.operationQueue cancelAllOperations];
+}
 @end
