dd969bf350a793e4c75098677888c00dee07e67f
hbj520 <hbjmailformac@163.com>
2016-07-05 13:37:46 +0800

    修改视频商城－－han

---
+++ FUYIFinance/FUYIFinance/Views/HomePage/VideoShopTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  VideoShopTableViewCell.h
+//  FUYIFinance
+//
+//  Created by youyou on 16/7/5.
+//  Copyright © 2016年 youyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VideoShopTableViewCell : UITableViewCell
+
+@end
---
+++ FUYIFinance/FUYIFinance/Views/HomePage/VideoShopTableViewCell.m
@@ -0,0 +1,26 @@
+//
+//  VideoShopTableViewCell.m
+//  FUYIFinance
+//
+//  Created by youyou on 16/7/5.
+//  Copyright © 2016年 youyou. All rights reserved.
+//
+
+#import "VideoShopTableViewCell.h"
+
+@implementation VideoShopTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+- (void)setValue:(id)value forUndefinedKey:(NSString *)key{
+    
+}
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
