159d923be770eac970dce78a5da3e8a94643f828
youyoumacmini3 <youyoumacmini3@youyoumacmini3demac-mini.local>
2016-07-13 09:47:38 +0800

    animation-wang

--- FUYIFinance/FUYIFinance/Controllers/HomePage/VideoDetailViewController.m
+++ FUYIFinance/FUYIFinance/Controllers/HomePage/VideoDetailViewController.m
@@ -16,21 +16,31 @@
 #import "StoreDataModel.h"
 
 #import "LabelHelper.h"
+#import "LPPopup.h"
 
 #import <SDWebImage/UIImageView+WebCache.h>
 
 @interface VideoDetailViewController ()<UITableViewDataSource,UITableViewDelegate>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
-
+@property (nonatomic,strong) UIBezierPath *path;
 
 @end
 
 @implementation VideoDetailViewController
+{
+    CALayer     *layer;
+    UILabel     *_cntLabel;// 购物车总数量显示文本
+    NSInteger    _cnt;// 总数量
+    
+    
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [self creatUI];
+      _cnt = 0;
 
 }
 
@@ -49,6 +59,29 @@
     [self.tableView registerNib:[UINib nibWithNibName:@"VideoDetailSecTableViewCell" bundle:nil] forCellReuseIdentifier:@"detailSecReuseID"];
     [self.tableView registerNib:[UINib nibWithNibName:@"VideoDetailThirdtTableViewCell" bundle:nil] forCellReuseIdentifier:@"detailThirdReuseID"];
     [self addBottomTapGesAndButton];//添加底部点击事件
+    
+//    // label
+//    _cntLabel = [[UILabel alloc] initWithFrame:CGRectMake(SCREEN_WIDTH-30, 25, 20, 20)];
+//    _cntLabel.textColor = customColor;
+//    _cntLabel.textAlignment = NSTextAlignmentCenter;
+//    _cntLabel.font = [UIFont boldSystemFontOfSize:13];
+//    _cntLabel.backgroundColor = [UIColor whiteColor];
+//    _cntLabel.layer.cornerRadius = CGRectGetHeight(_cntLabel.bounds)/2;
+//    _cntLabel.layer.masksToBounds = YES;
+//    _cntLabel.layer.borderWidth = 1.0f;
+//    _cntLabel.layer.borderColor = customColor.CGColor;
+//    [self.view addSubview:_cntLabel];
+//    
+//    if (_cnt == 0) {
+//        _cntLabel.hidden = YES;
+//    }
+//    
+//    _path = [UIBezierPath bezierPath];
+//    [_path moveToPoint:CGPointMake(50, SCREEN_HEIGHT * 0.7)];
+//    [_path addQuadCurveToPoint:CGPointMake(SCREEN_WIDTH-50-10, 5)
+//                  controlPoint:CGPointMake(156, 100)];
+    
+    
 }
 
 - (void)addBottomTapGesAndButton{
@@ -66,6 +99,75 @@
     [self.serviceView addGestureRecognizer:tapServiceGes];
 }
 
+//加入购物车
+- (void)addShopCarClick:(UIButton*)button{
+    
+    if (!layer)
+    {
+        UIColor *customColor  = [UIColor colorWithRed:237/255.0 green:20/255.0 blue:91/255.0 alpha:1.0f];
+        
+        button.enabled = NO;
+        layer = [CALayer layer];
+        layer.contents = (__bridge id)[UIImage imageNamed:@"test01.jpg"].CGImage;
+        layer.contentsGravity = kCAGravityResizeAspectFill;
+        layer.bounds = CGRectMake(0, 0, 50, 50);
+        
+        layer.masksToBounds = YES;
+        layer.position = CGPointMake(50, 150);
+        layer.borderColor = customColor.CGColor;
+        [self.view.layer addSublayer:layer];
+    }
+    
+    [self groupAnimation];
+}
+
+- (void)groupAnimation
+{
+    CAKeyframeAnimation *animation1 = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+    animation1.path = _path.CGPath;
+    animation1.rotationMode = kCAAnimationRotateAuto;
+    
+    CABasicAnimation *animation = [ CABasicAnimation
+                                   animationWithKeyPath: @"transform" ];
+    animation.fromValue = [NSValue valueWithCATransform3D:CATransform3DIdentity];
+    // 围绕Z轴旋转，垂直与屏幕
+    animation.toValue = [ NSValue valueWithCATransform3D:
+                         CATransform3DMakeRotation(M_PI, 0.0, 0.0, 1.0) ];
+    animation.duration = 0.15;
+    // 旋转效果累计，先转180度，接着再旋转180度，从而实现360旋转
+    animation.cumulative = YES;
+    animation.repeatCount = 1000;
+    
+    CABasicAnimation *expandAnimation = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+    expandAnimation.duration = 0.2f;
+    expandAnimation.beginTime = 0.3f;
+    expandAnimation.fromValue = [NSNumber numberWithFloat:0.3f];
+    expandAnimation.toValue = [NSNumber numberWithFloat:0.4f];
+    expandAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn];
+    
+    CABasicAnimation *narrowAnimation = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+    narrowAnimation.beginTime = 0.2f;
+    narrowAnimation.fromValue = [NSNumber numberWithFloat:0.7f];
+    narrowAnimation.duration = 0.3f;
+    narrowAnimation.toValue = [NSNumber numberWithFloat:0.9f];
+    narrowAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut];
+    
+    CAAnimationGroup *groups = [CAAnimationGroup animation];
+    groups.animations = @[animation1,expandAnimation,animation,narrowAnimation];
+    groups.duration = 0.6f;
+    groups.removedOnCompletion = NO;
+    groups.fillMode = kCAFillModeForwards;
+    groups.delegate = self;
+    [layer addAnimation:groups forKey:@"group"];
+}
+
+
+
+
+
+
+
+
 //收藏
 - (void)collectClick:(UIButton*)button{
     button.selected = !button.selected;
@@ -73,15 +175,6 @@
 
 }
 
-//加入购物车
-- (void)addShopCarClick:(UIButton*)button{
-    [UIView animateWithDuration:1.0 animations:^{
-       // _cartAnimView.frame=CGRectMake(self.screenWidth-55, -(self.screenHeight - CGRectGetHeight(self.view.frame) - 40), 0, 0);
-    } completion:^(BOOL finished) {
-      
-    }];
-    
-}
 
 //店铺
 - (void)shopClick:(UIGestureRecognizer *)ges{
