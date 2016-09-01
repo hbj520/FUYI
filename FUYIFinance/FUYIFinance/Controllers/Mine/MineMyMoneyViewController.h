3788f1d2f20149bae857d7782d66ca662d3ee470
zhangzhe <1659164165@qq.com>
2016-09-01 18:40:14 +0800

    修改 - 张

---
+++ FUYIFinance/FUYIFinance/Controllers/Mine/MineMyMoneyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MineMyMoneyViewController.h
+//  FUYIFinance
+//
+//  Created by 张哲 on 16/9/1.
+//  Copyright © 2016年 youyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MineMyMoneyViewController : UITableViewController
+
+@end
